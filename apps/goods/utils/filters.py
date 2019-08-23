99aed3e30e51ea65ec4e996f5b3c414c652d7f64
hj24 <huangjian24@foxmail.com>
2019-08-23 21:14:48 +0800

    自定义filter

--- apps/goods/utils/filters.py
+++ apps/goods/utils/filters.py
@@ -1,4 +1,5 @@
 import django_filters
+from rest_framework import filters
 
 from ..models import Goods
 
@@ -9,7 +10,15 @@ class GoodsFilter(django_filters.rest_framework.FilterSet):
     """
     price_min = django_filters.NumberFilter(field_name='shop_price', lookup_expr='gte')
     price_max = django_filters.NumberFilter(field_name='shop_price', lookup_expr='lte')
+    # 模糊查询，前缀i表示忽略大小写
+    name = django_filters.CharFilter(field_name='name', lookup_expr='icontains')
 
     class Meta:
         model = Goods
         fields = ['price_min', 'price_max']
+
+
+class OrderingFilter(filters.OrderingFilter):
+
+    ordering_param = 'o'
+    ordering_title = 'o'
